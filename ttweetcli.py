b7d2bb13df32b6bca906398d02def01660702a97
dreese33 <dylanjacobreese@gatech.edu>
2020-02-11 14:26:18 -0500

    Added more test scenarios and debugged them for potential client crashes

--- ttweetcli.py
+++ ttweetcli.py
@@ -1,5 +1,6 @@
 from socket import *
 import argparse
+import ipaddress
 
 
 def upload(client, message):
@@ -11,7 +12,7 @@ def upload(client, message):
 def download(client):
     client.send("DOWNLOAD".encode())
     response = client.recv(1024)
-    print(response.decode())
+    print("Message currently stored in server: " + response.decode())
 
 
 def getOptions():
@@ -24,30 +25,67 @@ def getOptions():
     return parser.parse_args()
 
 
+def validIP(address):
+    try:
+        ipaddress.ip_address(address)
+        return True
+    except:
+        return False
+
+
+print("\n")
+sendMessage = True
 options = getOptions()
-print(options.upload)
-print(options.download)
 
 serverName = options.serverIP   # '127.0.0.1'
+
+if not validIP(serverName):
+    print("Invalid IP Address Entered. Client Exiting...")
+    sendMessage = False
+
 serverPort = options.serverPort     # 13402
 clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.settimeout(3)
 
-clientSocket.connect((serverName, serverPort))
+
+# Attempts to connect to the server
+if sendMessage:
+    try:
+        clientSocket.connect((serverName, serverPort))
+    except ConnectionRefusedError:
+        print("Connection refused on host " + str(serverName) + ":" + str(serverPort))
+        sendMessage = False
+    except timeout:
+        print("Connection failed due to exceeding the 3 second timeout. "
+              "This is most likely due to an incorrect IP address entered. " "Client Exiting...")
+        sendMessage = False
+
+    except Exception as exc:
+        print("Exception occurred connecting to server: " + str(exc))
+        sendMessage = False
 
 
-if options.upload and options.download:
-    print("Cannot call both upload and download simultaneously...")
-elif options.upload:
-    if options.message is not None:
-        if len(options.message) <= 150:
-            upload(clientSocket, options.message)
+# Attempts to send message to the server
+if sendMessage:
+    try:
+        if options.upload and options.download:
+            print("Cannot call both upload and download simultaneously...")
+        elif options.upload:
+            if options.message is not None:
+                if len(options.message) <= 150:
+                    upload(clientSocket, options.message)
+                else:
+                    print("Cannot send message larger than 150 characters. Client exiting...")
+            else:
+                print("Please enter a message while uploading. Client exiting...")
+        elif options.download:
+            if options.message is not None:
+                print("Warning: Message " + "\'" + str(options.message) + "\'" +
+                      " will not be uploaded to the server in download mode.")
+            download(clientSocket)
         else:
-            print("Cannot send message larger than 150 characters. Client exiting...")
-    else:
-        print("Please enter a message while uploading. Client exiting...")
-elif options.download:
-    download(clientSocket)
-else:
-    print("Invalid argument.")
+            print("Invalid argument.")
+    except Exception as err:
+        print("Exception occurred exchanging messages with server: " + str(err))
 
 clientSocket.close()
